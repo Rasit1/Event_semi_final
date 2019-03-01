ab176f871fd0f45f0b381241bc4d2144d0cd07f4
rasit1 <rasitramiakatra@gmail.com>
2019-03-01 15:41:35 +0300

    files added

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'event#index'
+  get 'home/secret'
+  devise_for :users
+  resources :users
+  resources :avatars, only: [:create]
+  resources :event
+  resources :charges
+
+end
